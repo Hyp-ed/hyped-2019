6f76bf0d4a1aa23681ec8a604e444a63ca1f715d
Jam516 <kufuorkofi@gmail.com>
2019-01-15 11:00:00 +0000

    Add placeholder files

---
+++ Hyped/src/state_machine/main.hpp
@@ -0,0 +1,28 @@
+/*
+ * Author: 
+ * Organisation: HYPED
+ * Date: 
+ * Description:
+ * Main instantiates HypedMachine. It also monitors other data and generates Events
+ * for the HypedMachine. Note, StateMachine structure in Data is not updated here but
+ * in HypedMachine.
+ *
+ *    Copyright 2019 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+namespace hyped {
+
+namespace state_machine {
+
+}}  
