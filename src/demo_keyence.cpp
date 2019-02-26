269b097c5ea84de1300aa6ff228984d6162189a2
gregorydayao12 <s1814658@ed.ac.uk>
2019-02-26 23:59:23 +0000

    added timer for testing

--- src/demo_keyence.cpp
+++ src/demo_keyence.cpp
@@ -1,4 +1,4 @@
-// #include <stdio.h>
+#include <stdio.h>
 #include "utils/concurrent/thread.hpp"
 #include "utils/io/gpio.hpp"
 #include "sensors/gpio_counter.hpp"
@@ -6,41 +6,55 @@
 #include "utils/logger.hpp"
 #include "utils/timer.hpp"
 
-// using hyped::utils::concurrent::Thread;
+using hyped::utils::concurrent::Thread;
 using hyped::utils::io::GPIO;
+using hyped::sensors::GpioCounter;
 using hyped::utils::System;
 using hyped::utils::Logger;
 using hyped::utils::Timer;
-using hyped::sensors::GpioCounter;
 using hyped::data::StripeCounter;      // data.hpp
+
+using namespace hyped::data;
 namespace io = hyped::utils::io;
 
-constexpr int kStripeNum = 10;
+constexpr int kStripeNum = 50;
 constexpr uint64_t kCheckTime = 3150000;       //3.15 seconds
 
 int main(int argc, char* argv[]) { 
-  System::parseArgs(argc, argv);
+  hyped::utils::System::parseArgs(argc, argv);
   Logger log(true, 0);
 
   GPIO keyence_pin(67, io::gpio::kIn);           // 4 down on right
-  GpioCounter keyence(67);
+  GPIO test_68(68, io::gpio::kOut);          // 5 down on right
+
+  hyped::sensors::GpioCounter keyence(67);
 
   int stripe_count = 0;
   Timer timer;
   timer.reset();
   timer.start();
   uint64_t start_time = timer.getMicros();
+  test_68.clear();
+  int num = 0;
+
+  keyence.start();                        // start gpio_counter thread
 
   while(stripe_count<kStripeNum){
-    keyence.start();                        // start gpio_counter thread
+    if(num%2>0){          // to test with gpio pin
+      test_68.set();
+    }
+    else{
+      test_68.clear();
+    }
     if(timer.getMicros()-start_time>kCheckTime){      // if time between stripe suceeded
       stripe_count++;
       log.DBG("TEST-KEYENCE", "Missed stripe at time %f",timer.getMicros());
       start_time = timer.getMicros();
     }
-    StripeCounter stripe_data = keyence.getStripeCounter();       // to get value
+    StripeCounter stripe_data = keyence.hyped::sensors::GpioCounter::getStripeCounter();       // to get value
     stripe_count += stripe_data.count.value;
     log.DBG("TEST-KEYENCE", "Stripe hit at time %f",stripe_data.count.timestamp);
+
   }
 
   timer.stop();
