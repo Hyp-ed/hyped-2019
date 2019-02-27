88ec7b7aab80306b4dad09b5694eb14704b31a0c
gregorydayao12 <s1814658@ed.ac.uk>
2019-02-27 20:53:03 +0000

    datasheet added

--- src/demo_keyence.cpp
+++ src/demo_keyence.cpp
@@ -17,7 +17,7 @@ using hyped::data::StripeCounter;      // data.hpp
 using namespace hyped::data;
 namespace io = hyped::utils::io;
 
-constexpr int kStripeNum = 50;
+constexpr int kStripeNum = 15;
 constexpr uint64_t kCheckTime = 3150000;       //3.15 seconds
 
 int main(int argc, char* argv[]) { 
@@ -31,7 +31,8 @@ int main(int argc, char* argv[]) {
 
   GpioCounter keyence(38);        // 4 down on left
 
-  int stripe_count = 0;
+  uint32_t stripe_count = 0;
+  uint32_t prev_count = -1;
   Timer timer;      // get timer started
   timer.reset();
   timer.start();
@@ -43,12 +44,13 @@ int main(int argc, char* argv[]) {
   log.DBG("TEST-KEYENCE", "Thread started");
 
   Thread::sleep(500);
-  StripeCounter stripe_data;
-  int num = 0;
+  StripeCounter current_stripe_data;
+
+  int run_counter = 0;
   while(stripe_count<kStripeNum){             // so all of this runs while it waits
-    if(num%2==0){          // to test with gpio pin- I have come to the conclusion that GPIO pins do not help testing for me
+    if(run_counter%2==0){          // to test with gpio pin- I have come to the conclusion that GPIO pins do not help testing for me
       test_39.set();
-      log.DBG("TEST-KEYENCE", "Read GPIO pin val %d", test_39.read());
+      log.DBG("TEST-KEYENCE", "Read GPIO pin val %d", test_39.wait());
     }
     else{
       test_39.clear();
@@ -61,16 +63,25 @@ int main(int argc, char* argv[]) {
     //   start_time = timer.getMicros();
     // }
 
-    stripe_data = keyence.getStripeCounter();       // to get value     // not reading data
-    log.DBG("TEST-KEYENCE", "Stripe_data count: %f", stripe_data.count.value);
-    stripe_count += stripe_data.count.value;
-    log.INFO("TEST-KEYENCE", "Stripe hit at time %f",stripe_data.count.timestamp);      // 0 because no data!
-    Thread::sleep(500);
-    num++;
-    timer.stop();
-    log.DBG("TEST-KEYENCE", "Time: %f", timer.getMicros());
-    timer.start();
-    log.DBG("TEST-KEYENCE","num: %d", num);
+    current_stripe_data = keyence.getStripeCounter();       // to get value     // not reading data!!
+    stripe_count = current_stripe_data.count.value;
+    if(stripe_count==prev_count){     // same value as last time
+      // no stripe detected
+      log.INFO("TEST-KEYENCE", "No stripe detected!");  
+    }
+    else{
+      // has hit stripe
+      log.INFO("TEST-KEYENCE", "Stripe hit at time %f",current_stripe_data.count.timestamp);      // 0 because no data
+      prev_count = stripe_count;
+    }
+
+    log.DBG("TEST-KEYENCE", "Stripe_data count: %f", current_stripe_data.count.value);
+    Thread::sleep(100);
+    run_counter++;
+    // timer.stop();
+    // log.DBG("TEST-KEYENCE", "Time: %f", timer.getMicros());
+    // timer.start();
+    log.DBG("TEST-KEYENCE","run_counter: %d", run_counter);
 
   }
 
