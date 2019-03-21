ef6c12b151cdb36a0bce3003e85efdf1ab683766
gregorydayao12 <s1814658@ed.ac.uk>
2019-03-21 15:41:22 +0000

    config tester

--- src/demo_keyence_rpm.cpp
+++ src/demo_keyence_rpm.cpp
@@ -3,6 +3,7 @@
 #include "sensors/gpio_counter.hpp"
 #include "utils/logger.hpp"
 #include "utils/timer.hpp"
+#include "utils/system.hpp"
 
 using hyped::utils::concurrent::Thread;
 using hyped::utils::io::GPIO;
@@ -10,21 +11,21 @@ using hyped::sensors::GpioCounter;
 using hyped::utils::Logger;
 using hyped::utils::Timer;
 
-constexpr uint32_t kStripeNum = 10000;          // depending on configuration of run
+constexpr uint32_t kStripeNum = 1000;          // depending on configuration of run
 
-int main() {
-    Logger log;
+int main(int argc, char* argv[]) {
+    hyped::utils::System::parseArgs(argc, argv);
+    Logger log(true, 0);
     GPIO pin(66, hyped::utils::io::gpio::kIn);          // initialise gpio
 
     uint32_t stripe_count = 0;
     Timer timer;      // get timer started
     timer.reset();
     timer.start();
-
+    Thread::sleep(500);
     uint64_t start_time = timer.getTimeMicros();
     log.INFO("TEST-KEYENCE", "Start time: %f", start_time);
-    Thread::sleep(500);
-    uint8_t val = pin.wait();
+    uint8_t val;        // pin.wait();
 
     while (stripe_count < kStripeNum){
         log.DBG("KEYENCE-TEST","Waiting");
@@ -32,6 +33,7 @@ int main() {
         if (val == 1)                   // if hit stripe --> gpio to high
         {
             log.DBG("KEYENCE-TEST","Hit stripe at: %d micros",timer.getTimeMicros());
+            log.DBG("KEYENCE-TEST","Stripe Count: ",stripe_count);
             stripe_count++;
         }
     }
