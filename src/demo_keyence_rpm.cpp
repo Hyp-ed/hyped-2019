481c54a383b2de19a77c4490fde66cbee3aa9cea
gregorydayao12 <s1814658@ed.ac.uk>
2019-03-21 16:55:39 +0000

    not processing voltage change

--- src/demo_keyence_rpm.cpp
+++ src/demo_keyence_rpm.cpp
@@ -16,8 +16,8 @@ constexpr uint32_t kStripeNum = 1000;          // depending on configuration of 
 int main(int argc, char* argv[]) {
     hyped::utils::System::parseArgs(argc, argv);
     Logger log(true, 0);
-    GPIO pin(66, hyped::utils::io::gpio::kIn);          // initialise gpio
-
+    GPIO pin(69, hyped::utils::io::gpio::kIn);          // initialise gpio
+    
     uint32_t stripe_count = 0;
     Timer timer;      // get timer started
     timer.reset();
@@ -25,16 +25,16 @@ int main(int argc, char* argv[]) {
     Thread::sleep(500);
     uint64_t start_time = timer.getTimeMicros();
     log.INFO("TEST-KEYENCE", "Start time: %f", start_time);
-    uint8_t val;        // pin.wait();
+    uint8_t val = pin.wait();
 
     while (stripe_count < kStripeNum){
         log.DBG("KEYENCE-TEST","Waiting");
         val = pin.wait();
         if (val == 1)                   // if hit stripe --> gpio to high
         {
-            log.DBG("KEYENCE-TEST","Hit stripe at: %d micros",timer.getTimeMicros());
-            log.DBG("KEYENCE-TEST","Stripe Count: ",stripe_count);
+            // log.DBG("KEYENCE-TEST","Hit stripe at: %d micros",timer.getTimeMicros());
             stripe_count++;
+            log.DBG("KEYENCE-TEST","Stripe Count: %d",stripe_count);
         }
     }
 
