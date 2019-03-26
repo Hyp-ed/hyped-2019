f98e2d8f0a7d5d3e742255141cd90e2c9db9fe37
Reason1338 <gregor.konzett98@gmail.com>
2019-03-26 22:39:29 +0000

    Integrate Fake Controller into the StateProcessor

--- src/propulsion/state_processor.cpp
+++ src/propulsion/state_processor.cpp
@@ -28,14 +28,19 @@ StateProcessor::StateProcessor(int motorAmount, Logger &log)
     : log_(log),
       sys_(System::getSystem()),
       motorAmount(motorAmount),
-      initialized(false)
+      initialized(false),
+      criticalError(false),
+      servicePropulsionSpeed(10)
 {
     log_.DBG1("Motor", "StateProcessor constructor was called");
-    useTestControllers = false;  // sys_.fake_motors;
+    useTestControllers = true;  // sys_.fake_motors;
 
     // controllers = new ControllerInterface[motorAmount];
 
-    if (useTestControllers) {  // Use the test controller implementation
+    if (useTestControllers) {  // Use the test controllers implementation
+        for (int i = 0; i < motorAmount; i++) {
+            controllers[i] = new FakeController(log_, i, false);
+        }
     } else {  // Use real controllers
         for (int i = 0; i < motorAmount; i++) {
             // controllers[i] = new Controller();
@@ -48,53 +53,102 @@ void StateProcessor::initMotors()
     // Register controllers on CAN bus
     registerControllers();
 
-    // Configure controller parameters
+    // Configure controllers parameters
     configureControllers();
 
-    // TODO(gregor): Handle errors
+    bool error = false;
 
-    // If no error
-    prepareMotors();
+    for (int i = 0;i < motorAmount;i++) {
+        if (controllers[i]->getFailure()) {
+            error = true;
+            break;
+        }
+    }
 
-    initialized = true;
+    if (error) {
+        prepareMotors();
+        initialized = true;
+    } else {
+        criticalError = true;
+    }
 }
 
 void StateProcessor::registerControllers()
 {
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->registerController();
+    }
 }
 
 void StateProcessor::configureControllers()
 {
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->configure();
+    }
 }
 
 void StateProcessor::prepareMotors()
 {
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->enterOperational();
+    }
 }
 
 void StateProcessor::enterPreOperational()
 {
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->enterPreOperational();
+    }
 }
 
 void StateProcessor::accelerate()
 {
+    int speed = 0;
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->sendTargetVelocity(speed);
+    }
 }
 
 void StateProcessor::quickStopAll()
 {
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->quickStop();
+    }
 }
 
 void StateProcessor::healthCheck()
 {
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->healthCheck();
+    }
 }
 
 bool StateProcessor::getFailure()
 {
+    for (int i = 0;i < motorAmount; i++) {
+        if (controllers[i]->getFailure()) {
+            return true;
+        }
+    }
+
     return false;
+}
+
+void StateProcessor::servicePropulsion()
+{
+    for (int i = 0;i < motorAmount; i++) {
+        controllers[i]->sendTargetVelocity(servicePropulsionSpeed);
+    }
 }
 
 bool StateProcessor::isInitialized()
 {
     return this->initialized;
+}
+
+bool StateProcessor::isCriticalFailure()
+{
+    return this->criticalError;
 }
 
 }  // namespace motor_control
