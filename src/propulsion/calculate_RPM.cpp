0e9467250ed916fcd96d447540d3a98351cb598d
George Ka <georgeka@pc-68-29.customer.haddingtonplace.crm.lan>
2019-03-31 17:40:26 +0100

    finished 1st prototype

---
+++ src/propulsion/calculate_RPM.cpp
@@ -0,0 +1,149 @@
+/*
+ * Author: George Karabassis
+ * Organisation: HYPED
+ * Date: 30/3/2018
+ * Description:
+ *
+ *    Copyright 2018 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *    except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "propulsion/calculate_RPM.hpp"
+
+#include <math.h>
+#ifndef M_PI
+#define M_PI           3.14159265358979323846
+#endif
+
+#include <cstdint>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+// #include "motor_control/communicator.hpp"
+#include "data/data.hpp"
+#include "utils/system.hpp"
+
+namespace hyped {
+
+using data::NavigationType;
+using utils::System;
+using utils::Timer;
+
+namespace motor_control {
+
+constexpr double  kHalbachRadius    = 0.148;
+const std::string kAccelerationData = "../BeagleBone_black/data/configuration/AccelerationSlip.txt";
+
+CalculateRPM::CalculateRPM(Logger& log): 
+    data_(data::Data::getInstance()),
+    log_(log),
+    motor_failure_(false),
+    slip_calculated_(false)
+{
+    state_      = data_.getStateMachineData();
+    motor_data_.module_status = data::ModuleStatus::kStart;
+    motor_data_.velocity_1 = 0;
+    motor_data_.velocity_2 = 0;
+    motor_data_.velocity_3 = 0;
+    motor_data_.velocity_4 = 0;
+    data_.setMotorData(motor_data_);
+    motor_velocity_ = {0, 0, 0, 0};
+    //communicator_ = new Communicator(log);
+}
+
+    void CalculateRPM::updateMotorFailure(){
+        log_.ERR("MOTOR", "Motor State: MOTOR FAILURE\n");
+        motor_data_ = data_.getMotorData();
+        motor_data_.module_status = data::ModuleStatus::kCriticalFailure;
+        data_.setMotorData(motor_data_);
+        motor_failure_ = true;
+    }
+
+    std::vector<std::vector<double>> CalculateRPM::transpose(std::vector<std::vector<double>> data){
+        std::vector<std::vector<double>> transpose(2, std::vector<double>(data.size(), 1));
+        for (uint16_t i = 0; i < data.size(); ++i) {
+            for (uint16_t j = 0; j < 2; ++j) {
+            transpose[j][i] = data[i][j];
+            }
+        }
+        return transpose; 
+    }
+
+    bool CalculateRPM::check_file(std::string filepath){
+
+        std::ifstream data;
+        data.open(filepath);
+
+        if (!data.is_open()) {
+            log_.ERR("MOTOR", "Could not open file: %s", filepath.c_str());
+            updateMotorFailure();
+            return false;
+        } else if (filepath == kAccelerationData) {
+            log_.INFO("MOTOR", "Calculating acceleration slip...");
+        } else {
+            log_.INFO("MOTOR", "Calculating deceleration slip...");
+        }
+
+        return true;
+
+    };
+
+    void CalculateRPM::RPM_calc(std::string filepath){
+
+        double slip, translational_velocity, angular_velocity, rpm;
+        std::ifstream data;
+        data.open(filepath);
+
+        std::string line;
+        while (std::getline(data, line)) {
+
+            std::string split_line;
+            std::vector<double> temp_vec;
+            std::stringstream ss(line);
+            while (std::getline(ss, split_line, '\t')) {
+                temp_vec.push_back(std::move(stod(split_line)));
+            }
+
+            // Calculate angular velocity from slip, translational velocity and halbach wheel radius
+            slip = temp_vec[0];
+            translational_velocity = temp_vec[1];
+            angular_velocity = (slip + translational_velocity) / kHalbachRadius;
+
+            // Calculate RPM given calculated angular velocity
+            rpm = (angular_velocity * 60) / (2*M_PI);
+
+            // Use temporary vector to hold translational velocity and rpm
+            temp_vec[0] = translational_velocity;
+            temp_vec[1] = rpm;
+
+            // Add data to appropriate container
+            if (filepath == kAccelerationData) {
+                acceleration_slip_.push_back(temp_vec);
+            } 
+            else {
+                deceleration_slip_.push_back(temp_vec);
+            }
+        }
+
+        // If both containers have been populated, set bool to true
+        if (!acceleration_slip_.empty() && !deceleration_slip_.empty()) {
+            acceleration_slip_ = transpose(acceleration_slip_);
+            deceleration_slip_ = transpose(deceleration_slip_);
+            slip_calculated_ = true;
+            log_.INFO("MOTOR", "All slip values calculated");
+        }
+
+    };
+  }
+}
---
+++ src/propulsion/calculate_RPM.hpp
@@ -0,0 +1,87 @@
+/*
+ * Author: George Karabassis
+ * Organisation: HYPED
+ * Date: 30/3/2018
+ * Description:
+ *
+ *    Copyright 2018 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *    except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef BEAGLEBONE_BLACK_MOTOR_CONTROL_MAIN_HPP_
+#define BEAGLEBONE_BLACK_MOTOR_CONTROL_MAIN_HPP_
+
+#include <cstdint>
+#include <vector>
+#include <string>
+
+// #include "motor_control/communicator.hpp"
+#include "propulsion/state_processor_interface.hpp"
+#include "utils/concurrent/thread.hpp"
+#include "utils/concurrent/barrier.hpp"
+#include "data/data.hpp"
+#include "utils/timer.hpp"
+#include "utils/logger.hpp"
+
+namespace hyped {
+
+using data::NavigationType;
+using utils::concurrent::Thread;
+using utils::concurrent::Barrier;
+using utils::Logger;
+using utils::Timer;
+
+namespace motor_control {
+
+class CalculateRPM{
+
+    public:
+
+        CalculateRPM(Logger& log);
+
+    private:
+  /**
+   *   @brief  { Reads slip and translational velocity data from acceleration and
+   *             deceleration text files, calculates RPM's for appropriate slip at each
+   *             translational velocity and stores the values in a 2D array containing
+   *             translational velocity and RPM }
+   */
+
+        bool check_file(std::string filepath);
+
+        void updateMotorFailure();
+
+        std::vector<std::vector<double>> transpose(std::vector<std::vector<double>> data);
+
+        // int32_t RPM_calc(int32_t slip_calculated_);
+        void RPM_calc(std::string filepath);
+
+        data::Data& data_;
+        data::StateMachine state_;
+        data::Motors motor_data_;
+        MotorVelocity motor_velocity_;
+
+        std::vector<std::vector<double>> acceleration_slip_;
+        std::vector<std::vector<double>> deceleration_slip_;
+
+        Logger& log_;
+
+        bool motor_failure_;
+        bool slip_calculated_;
+        // int32_t slip_velocity;
+};
+
+}}  // namespace hyped::motor_control
+
+#endif  // BEAGLEBONE_BLACK_MOTOR_CONTROL_MAIN_HPP_
+
+
+
