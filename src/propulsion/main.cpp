b8c091a4783978bbda19a174c31649f90cd2dd1e
Gregor Konzett <gregor.konzett98@gmail.com>
2019-03-31 23:00:23 +0100

    remove test code from main.cpp

--- src/propulsion/main.cpp
+++ src/propulsion/main.cpp
@@ -40,11 +40,11 @@ void Main::run()
     System &sys = System::getSystem();
 
     Data stateMachineData = Data::getInstance();
-    currentState = State::kCalibrating;
+
     while (isRunning && sys.running_) {
         // Get the current state of the system from the state machine's data
-        // currentState = stateMachineData.getStateMachineData().current_state;
 
+        currentState = stateMachineData.getStateMachineData().current_state;
 
         if (currentState == State::kIdle) {  // Initialize motors
             log_.INFO("Motor", "State idle");
@@ -61,8 +61,6 @@ void Main::run()
                     isRunning = false;
                 }
             }
-
-            currentState = State::kAccelerating;
 
             yield();
         } else if (currentState == State::kReady) {
@@ -103,8 +101,6 @@ void Main::run()
             isRunning = false;
             stateProcessor->quickStopAll();
         }
-
-        sleep(1000);
     }
 
     log_.INFO("Motor", "Thread shutting down");
