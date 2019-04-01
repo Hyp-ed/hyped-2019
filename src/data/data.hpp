c778e2259e9d9549174018b89db43453b05bf487
Neil Weidinger <neilweidingern@gmail.com>
2019-04-01 12:18:26 +0100

    changed variable names in data.hpp and data.cpp from comms to telem

--- src/data/data.hpp
+++ src/data/data.hpp
@@ -126,10 +126,10 @@ struct Motors : public Module {
 };
 
 // -------------------------------------------------------------------------------------------------
-// Communications data
+// Telemetry data
 // -------------------------------------------------------------------------------------------------
 
-struct Communications : public Module {
+struct Telemetry : public Module {
   bool launch_command;
   bool reset_command;
   float run_length;  // m
@@ -269,12 +269,12 @@ class Data {
   /**
    * @brief      Retrieves data on whether stop/kill power commands have been issued.
    */
-  Communications getCommunicationsData();
+  Telemetry getTelemetryData();
 
   /**
    * @brief      Should be called to update communications data.
    */
-  void setCommunicationsData(const Communications& communications_data);
+  void setTelemetryData(const Telemetry& telemetry_data);
 
  private:
   StateMachine state_machine_;
@@ -282,7 +282,7 @@ class Data {
   Sensors sensors_;
   Motors motors_;
   Batteries batteries_;
-  Communications communications_;
+  Telemetry telemetry_;
   SensorCalibration calibration_data_;
   EmergencyBrakes emergency_brakes_;
   int temperature_;  // In degrees C
@@ -295,7 +295,7 @@ class Data {
   Lock lock_motors_;
   Lock lock_temp_;
 
-  Lock lock_communications_;
+  Lock lock_telemetry_;
   Lock lock_batteries_;
   Lock lock_emergency_brakes_;
   Lock lock_calibration_data_;
