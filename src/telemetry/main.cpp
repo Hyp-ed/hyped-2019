73c40e02aefb77cc4dea558056727eb6b54e9cce
Neil Weidinger <neilweidingern@gmail.com>
2019-04-04 02:24:56 +0100

    shared telem struct should now be fully capable of being updated with data from server

--- src/telemetry/main.cpp
+++ src/telemetry/main.cpp
@@ -26,6 +26,7 @@
 namespace hyped {
 
 using client::Client;
+using data::ModuleStatus;
 using data::Data;
 using data::Telemetry;
 
@@ -52,8 +53,11 @@ void Main::run()
 
     while (true) {
         Telemetry telem_data = data_.getTelemetryData();
+        log_.DBG2("Telemetry", "SHARED module_status: %d", telem_data.module_status);
         log_.DBG2("Telemetry", "SHARED launch_command: %s", telem_data.launch_command ? "true" : "false"); // NOLINT
+        log_.DBG2("Telemetry", "SHARED reset_command: %s", telem_data.reset_command ? "true" : "false"); // NOLINT
         log_.DBG2("Telemetry", "SHARED run_length: %f", telem_data.run_length);
+        log_.DBG2("Telemetry", "SHARED service_propulsion_go: %s", telem_data.service_propulsion_go ? "true" : "false"); // NOLINT
 
         telemetry_data::TestMessage msg;
 
@@ -91,25 +95,42 @@ void Main::recvLoop()
 {
     telemetry_data::ServerToClient msg;
     // not sure whether to put this in or ouside of loop
-    Telemetry shared_telem_data = data_.getTelemetryData();
+    Telemetry telem_data_struct = data_.getTelemetryData();
 
     while (true) {
         msg = client.receiveData();
 
         switch (msg.command()) {
+            case telemetry_data::ServerToClient::ACK:
+                log_.DBG1("Telemetry", "FROM SERVER: ACK");
+                break;
+            case telemetry_data::ServerToClient::STOP:
+                log_.DBG1("Telemetry", "FROM SERVER: STOP");
+                telem_data_struct.module_status = ModuleStatus::kCriticalFailure;
+                break;
             case telemetry_data::ServerToClient::LAUNCH:
                 log_.DBG1("Telemetry", "FROM SERVER: LAUNCH");
-                shared_telem_data.launch_command = true;
+                telem_data_struct.launch_command = true;
+                break;
+            case telemetry_data::ServerToClient::RESET:
+                log_.DBG1("Telemetry", "FROM SERVER: RESET");
+                telem_data_struct.reset_command = true;
                 break;
             case telemetry_data::ServerToClient::RUN_LENGTH:
                 log_.DBG1("Telemetry", "FROM SERVER: RUN_LENGTH %f", msg.run_length());
-                shared_telem_data.run_length = msg.run_length();
+                telem_data_struct.run_length = msg.run_length();
+                break;
+            case telemetry_data::ServerToClient::SERVICE_PROPULSION:
+                log_.DBG1("Telemetry", "FROM SERVER: SERVICE_PROPULSION %s", msg.service_propulsion() ? "true" : "false");  // NOLINT
+                telem_data_struct.service_propulsion_go = msg.service_propulsion();
                 break;
             default:
-                log_.ERR("Telemetry", "Received unrecognized input from server");
+                log_.ERR("Telemetry", "Unrecognized input from server, entering critical failure");
+                telem_data_struct.module_status = ModuleStatus::kCriticalFailure;
+                break;
         }
 
-        data_.setTelemetryData(shared_telem_data);
+        data_.setTelemetryData(telem_data_struct);
     }
 }
 
