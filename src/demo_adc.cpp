e61571629cb0762c93d282a91dd62e791e027b6c
gregorydayao12 <s1814658@ed.ac.uk>
2019-05-14 11:59:31 +0100

    demo and src config

---
+++ src/demo_adc.cpp
@@ -0,0 +1,28 @@
+
+// #include <stdio.h>
+#include "utils/concurrent/thread.hpp"
+#include "utils/io/adc.hpp"
+#include "utils/system.hpp"
+#include "utils/logger.hpp"
+#include "utils/timer.hpp"
+
+// using hyped::utils::concurrent::Thread;
+using hyped::utils::io::ADC;
+using hyped::utils::System;
+using hyped::utils::Logger;
+using hyped::utils::Timer;
+using hyped::utils::concurrent::Thread;
+namespace io = hyped::utils::io;
+
+int main(int argc, char* argv[]) { 
+  System::parseArgs(argc, argv);
+  Logger log(true, 0);
+  
+  ADC analog(0, log);
+  Thread::sleep(100);
+
+  for (int i=0; i<20; i++) {
+    analog.read();
+  }
+  
+}
