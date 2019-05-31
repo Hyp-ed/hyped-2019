Hyp-ed/hyped-2019

commit 210154fcf32c51e8400e3c78cd5e62d8f49e402a

avoid unnecessary constructing and flushing protobuf output streams all the time

src/telemetry/client.cpp
@@ -75,13 +75,15 @@ bool Client::connect()
 
   log_.INFO("Telemetry", "Connected to server");
 
-  socket_stream_ = new google::protobuf::io::FileInputStream(sockfd_);
+  socket_stream_in_ = new google::protobuf::io::FileInputStream(sockfd_);
+  socket_stream_out_ = new google::protobuf::io::FileOutputStream(sockfd_);
   return true;
 }
 
 Client::~Client()
 {
-  delete socket_stream_;
+  delete socket_stream_out_;
+  delete socket_stream_in_;
   close(sockfd_);
 }
 
@@ -90,7 +92,7 @@ bool Client::sendData(telemetry_data::ClientToServer message)
   using namespace google::protobuf::util;
   log_.DBG3("Telemetry", "Starting to send message to server");
 
-  if (!SerializeDelimitedToFileDescriptor(message, sockfd_)) {
+  if (!SerializeDelimitedToZeroCopyStream(message, socket_stream_out_)) {
     log_.ERR("Telemetry", "SerializeDelimitedToFileDescriptor didn't work");
     return false;
   }
@@ -107,7 +109,7 @@ telemetry_data::ServerToClient Client::receiveData()
   telemetry_data::ServerToClient messageFromServer;
   log_.DBG1("Telemetry", "Waiting to receive from server");
 
-  if (!ParseDelimitedFromZeroCopyStream(&messageFromServer, socket_stream_, NULL)) {
+  if (!ParseDelimitedFromZeroCopyStream(&messageFromServer, socket_stream_in_, NULL)) {
     log_.ERR("Telemetry", "ParseDelimitedFromZeroCopyStream didn't work");
     // TODO(neil): probably throw exception here or something
   }

src/telemetry/client.hpp
@@ -31,6 +31,7 @@ namespace hyped {
 
 using utils::Logger;
 using google::protobuf::io::ZeroCopyInputStream;
+using google::protobuf::io::ZeroCopyOutputStream;
 
 namespace telemetry {
 
@@ -49,8 +50,10 @@ class Client {
   private:
     int sockfd_;
     Logger& log_;
-    // socket_stream_ is member var bc need to keep reading from same stream
-    ZeroCopyInputStream* socket_stream_;
+    // socket_stream_in_ is member var bc need to keep reading from same stream
+    ZeroCopyInputStream* socket_stream_in_;
+    // socket_stream_out_ is member var to avoid constructing and flushing streams all the time
+    ZeroCopyOutputStream* socket_stream_out_;
     SignalHandler signal_handler_;
 };
 

commit 82a137050daa3ae30f1083bfad6b59c808079828

fixes error introduced in commit 210154f where protobufs would buffer the output stream and messages wouldn't be sent immediately to the base-station

src/telemetry/client.cpp
@@ -19,7 +19,6 @@
  */
 
 #include <google/protobuf/util/delimited_message_util.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -96,6 +95,10 @@ bool Client::sendData(telemetry_data::ClientToServer message)
     log_.ERR("Telemetry", "Error sending message");
     return false;  // probs change to throwing exception
   }
+
+  // we have to call Flush() here otherwise protobufs will buffer the file output stream
+  // and the message will not be sent immediately like we would like
+  socket_stream_out_->Flush();
 
   log_.DBG3("Telemetry", "Finished sending message to server");
 

src/telemetry/client.hpp
@@ -21,7 +21,7 @@
 #ifndef TELEMETRY_CLIENT_HPP_
 #define TELEMETRY_CLIENT_HPP_
 
-#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <string>
 #include "telemetry/signalhandler.hpp"
 #include "telemetry/telemetrydata/message.pb.h"
@@ -30,8 +30,8 @@
 namespace hyped {
 
 using utils::Logger;
-using google::protobuf::io::ZeroCopyInputStream;
-using google::protobuf::io::ZeroCopyOutputStream;
+using google::protobuf::io::FileInputStream;
+using google::protobuf::io::FileOutputStream;
 
 namespace telemetry {
 
@@ -51,9 +51,9 @@ class Client {
     int sockfd_;
     Logger& log_;
     // socket_stream_in_ is member var bc need to keep reading from same stream
-    ZeroCopyInputStream* socket_stream_in_;
+    FileInputStream* socket_stream_in_;
     // socket_stream_out_ is member var to avoid constructing and flushing streams all the time
-    ZeroCopyOutputStream* socket_stream_out_;
+    FileOutputStream* socket_stream_out_;
     SignalHandler signal_handler_;
 };
 